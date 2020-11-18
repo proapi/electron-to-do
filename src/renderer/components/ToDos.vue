c792b5c13274a55f2bd5e611b0f65792a149f717
Michał Pawelski <kontakt@proapi.eu>
2020-11-18 18:52:58 +0100

    chore: initial commit

---
+++ src/renderer/components/ToDos.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>ToDo Electron Application</h1>
+
+    <div>
+      <div>
+        <div>
+          <button @click="fetchTodos()" class="btn btn-primary">Fetch Todos</button>
+        </div>
+      </div>
+      <div>
+        <div>
+          <ul>
+            <li v-for="todo in todos" :key="todo.id">{{todo.title}}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  const axios = require('axios')
+  export default {
+    name: 'ToDos',
+    data: () => {
+      return {
+        todos: []
+      }
+    },
+    methods: {
+      async fetchTodos () {
+        axios
+          .get('http://localhost:3001/')
+          .then(response => {
+            this.todos = response.data
+          })
+          .catch(error => {
+            if (error) throw new Error(error)
+          })
+      }
+    }
+  }
+</script>
---
+++ src/renderer/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'todos-page',
+      component: require('@/components/ToDos').default
+    },
+    {
+      path: '/landing',
+      name: 'landing-page',
+      component: require('@/components/LandingPage').default
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ]
+})
